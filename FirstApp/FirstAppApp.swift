8c048b33a061d40a28b2a99e088eda47f912a465
peter pan <apppeterpan@gmail.com>
2025-05-24 15:44:06 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by SHIH-YING PAN on 2025/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
